e28f73a8b06baa53b820ff2939068bb76636d8c9
bolot6 <kurmanaliev78@gmail.com>
2023-11-27 22:57:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const url = "https://65642499ceac41c0761d7f40.mockapi.io/todo";
+
+const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [todoAdd, setTodoAdd] = useState("");
+    const [editingTodo, setEditingTodo] = useState(null);
+    const [editedTodoTitle, setEditedTodoTitle] = useState("");
+
+    useEffect(() => {
+        axios(url).then(({ data }) => setTodos(data));
+    }, []);
+
+    const handleAddTodo = () => {
+        const newTodo = {
+            title: todoAdd,
+            completed: false,
+            createdAt: null,
+            completedAt: +new Date(),
+        };
+        setTodoAdd("");
+        axios.post(url, newTodo).then(({ data }) => setTodos([...todos, data]));
+    };
+
+    const handleDeleteTodo = (todo) => {
+        axios.delete(`${url}/${todo.id}`)
+            .then(({ data }) => setTodos(todos.filter((t) => t.id !== data.id)))
+            .catch((error) => console.error("Error deleting todo:", error));
+    };
+
+    const handleEditTodo = (todo) => {
+        setEditingTodo(todo.id);
+        setEditedTodoTitle(todo.title);
+    };
+
+    const handleSaveEdit = () => {
+        axios.put(`${url}/${editingTodo}`, { title: editedTodoTitle })
+            .then(() => {
+                setEditingTodo(null);
+                setTodos(todos.map((t) => (t.id === editingTodo ? { ...t, title: editedTodoTitle } : t)));
+            })
+            .catch((error) => console.error("Error editing todo:", error));
+    };
+
+    return (
+        <div className={'container'}>
+            <h1>Todo List</h1>
+            <input className={'inputMain'} type="text" onChange={(e) => setTodoAdd(e.target.value)} />
+            <button className={'mainButton'} onClick={handleAddTodo}>Add</button>
+            <div>
+                {todos.map((todo) => (
+                    <div className={'todo-wrapper'} key={todo.id}>
+                        {editingTodo === todo.id ? (
+                            <>
+                                <input type="text" value={editedTodoTitle} onChange={(e) => setEditedTodoTitle(e.target.value)} />
+                                <button onClick={handleSaveEdit}>Save</button>
+                            </>
+                        ) : (
+                            <>
+                                <p>{todo.title}</p>
+                                <input type="checkbox" checked={todo.completed} />
+                                <span>{dayjs(todo.completedAt).format('HH:mm MM/DD/YYYY')}</span>
+                                <button onClick={() => handleEditTodo(todo)}>Edit</button>
+                                <button onClick={() => handleDeleteTodo(todo)}>Delete</button>
+                            </>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
